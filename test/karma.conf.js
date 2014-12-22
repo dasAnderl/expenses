35566bec96b787546d1716bcbac3c31011846cfd
dasanderl <an.anderl@gmail.com>
2014-12-22 16:14:14 +0100

    grunt wiredep updating karma

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,6 +19,16 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
 
+      //'bower_components/angular/angular.js',
+      //'bower_components/angular-mocks/angular-mocks.js',
+      //'bower_components/angular-animate/angular-animate.js',
+      //'bower_components/angular-cookies/angular-cookies.js',
+      //'bower_components/angular-resource/angular-resource.js',
+      //'bower_components/angular-route/angular-route.js',
+      //'bower_components/angular-sanitize/angular-sanitize.js',
+      //'bower_components/angular-touch/angular-touch.js',
+      //'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      //'bower_components/angular-xeditable/dist/js/xeditable.js',
       //'app/scripts/**/*.js',
       //'test/mock/**/*.js',
       //'test/spec/**/*.js'
@@ -36,6 +46,12 @@ module.exports = function(config) {
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-xeditable/dist/js/xeditable.js',
       // endbower
+
+      //dev deps (bower.json) currently not wired
+      'bower_components/angular-mocks/angular-mocks.js',
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
