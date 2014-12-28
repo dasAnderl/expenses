9df0cdb1442f16d27e8327652471427925a2d207
dasanderl <an.anderl@gmail.com>
2014-12-28 17:34:44 +0100

    added ngStorage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -38,16 +38,8 @@ module.exports = function(config) {
       // during the grunt wiredep task (dev deps currently not supported)
       // bower:js
       'bower_components/jquery/dist/jquery.js',
-      'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'bower_components/angular-xeditable/dist/js/xeditable.js',
+      'bower_components/ngstorage/ngStorage.js',
       // endbower
 
       //dev deps (bower.json) currently not wired by wiredep
